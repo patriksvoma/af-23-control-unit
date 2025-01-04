66c1cd4f863d63e4290a33d7d0ad17969d3d54ef
Patzlyk <50177915+Patzlyk@users.noreply.github.com>
2025-01-04 22:11:44 +0100

    Created the read and write methods for the flash module

--- src/memory.cpp
+++ src/memory.cpp
@@ -10,28 +10,27 @@
 #define SPI0_SCK 2
 #define SPI0_CS 1
 
-// SPI commands for W25Q32BV
-#define COMMAND_READ_JEDEC_ID 0x9F  // Command to read JEDEC ID
-#define COMMAND_READ_UNIQUE_ID 0x4B // Command to read unique ID
-
 namespace memory
 {
-    void enable_cs();
-    void disable_cs();
+    void enableCs();
+    void disableCs();
     void init();
-    void read_jedec_id();
-    void read_unique_id();
+    void readJedecId();
+    void readUniqueId();
+    void enableWrite();
+    void writeData(uint32_t address, uint8_t data);
+    uint8_t readData(uint32_t address);
 
     SPISettings spi_settings(10000, MSBFIRST, SPI_MODE0);
 
     /// @brief Enables the CS pin (by pulling it low)
-    void enable_cs()
+    void enableCs()
     {
         digitalWrite(SPI0_CS, LOW);
     }
 
     /// @brief Disables the CS pin (by pulling it high)
-    void disable_cs()
+    void disableCs()
     {
         digitalWrite(SPI0_CS, HIGH);
     }
@@ -51,23 +50,33 @@ namespace memory
         // Start SPI
         SPI.begin();
 
-        read_jedec_id();
-        read_unique_id();
+        readJedecId();
+        readUniqueId();
+
+        Serial.println("Writing \"0xBA\" on address 0x0");
+
+        writeData(0x0, 0xBA);
+
+        Serial.println("Reading address 0x0...");
+
+        Serial.println(readData(0x0));
     }
 
     /// @brief Reads the JEDEC ID of the flash memory
-    void read_jedec_id()
+    void readJedecId()
     {
+        uint8_t command = 0x9F;
+
         Serial.print("Flash JEDEC ID: ");
 
         // Begin the transaction
         SPI.beginTransaction(spi_settings);
-        enable_cs();
+        enableCs();
 
         uint8_t out;
 
         // Send the command
-        SPI.transfer(COMMAND_READ_JEDEC_ID);
+        SPI.transfer(command);
 
         // Read the response
         out = SPI.transfer(0);
@@ -80,23 +89,25 @@ namespace memory
         Serial.println(out, HEX);
 
         // End the transaction
-        disable_cs();
+        disableCs();
         SPI.endTransaction();
     }
 
     /// @brief Reads the 64-bit unique ID of the memory
-    void read_unique_id()
+    void readUniqueId()
     {
+        uint8_t command = 0x4B;
+
         Serial.print("Flash unique ID: ");
 
         // Begin the transaction
         SPI.beginTransaction(spi_settings);
-        enable_cs();
+        enableCs();
 
         uint8_t out;
 
         // Send the command
-        SPI.transfer(COMMAND_READ_UNIQUE_ID);
+        SPI.transfer(command);
 
         // Send dummy bytes
         for (int i = 0; i < 4; i++)
@@ -115,21 +126,102 @@ namespace memory
         Serial.println();
 
         // End the transaction
-        disable_cs();
+        disableCs();
         SPI.endTransaction();
     }
 
-    /// @brief Not to be used, this is how a command should look like, probably
-    void command_template()
+    /// @brief Disables the write protection for the next write
+    void enableWrite()
     {
+        uint8_t command = 0x06;
+
         // Begin the transaction
         SPI.beginTransaction(spi_settings);
-        enable_cs();
+        enableCs();
+
+        // Send the command
+        SPI.transfer(command);
+        
+        // End the transaction
+        disableCs();
+        SPI.endTransaction();
+    }
+
+    /// @brief Writes a byte of data to the flash
+    /// @param address 24-bit address of the data
+    /// @param data Data byte
+    void writeData(uint32_t address, uint8_t data)
+    {
+        // Disable the write protection
+        enableWrite();
+
+        uint8_t command = 0x02;
+
+        // Begin the transaction
+        SPI.beginTransaction(spi_settings);
+        enableCs();
+
+        // Send the command
+        SPI.transfer(command);
+        
+        // Send the address from which the read operation should start
+        SPI.transfer((uint8_t)(address >> 16));
+        SPI.transfer((uint8_t)(address >> 8));
+        SPI.transfer((uint8_t)(address));
+
+        // Send the data
+        SPI.transfer(data);
+        
+        // End the transaction
+        disableCs();
+        SPI.endTransaction();   
+    }
+
+    /// @brief Reads a byte of data
+    /// @param address 24-bit address of the data byte
+    /// @return Data byte
+    uint8_t readData(uint32_t address)
+    {
+        uint8_t command = 0x03;
+        uint8_t ret;
+
+        // Begin the transaction
+        SPI.beginTransaction(spi_settings);
+        enableCs();
 
         uint8_t out;
 
         // Send the command
-        SPI.transfer(COMMAND_READ_JEDEC_ID);
+        SPI.transfer(command);
+
+        // Send the address from which the read operation should start
+        SPI.transfer((uint8_t)(address >> 16));
+        SPI.transfer((uint8_t)(address >> 8));
+        SPI.transfer((uint8_t)(address));
+
+        // Read the response
+        ret = SPI.transfer(0);
+
+        // End the transaction
+        disableCs();
+        SPI.endTransaction();
+
+        return ret;
+    }
+
+    /// @brief Not to be used, this is how a command should look like, probably
+    void commandTemplate()
+    {
+        uint8_t command = 0x00;
+
+        // Begin the transaction
+        SPI.beginTransaction(spi_settings);
+        enableCs();
+
+        uint8_t out;
+
+        // Send the command
+        SPI.transfer(command);
 
         // Send dummy bytes
         SPI.transfer(0);
@@ -139,7 +231,7 @@ namespace memory
         Serial.println(out, HEX);
         
         // End the transaction
-        disable_cs();
+        disableCs();
         SPI.endTransaction();
     }
 }
